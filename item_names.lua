7c6ddd3e040ff18994016d6125d9d7b36fbed865
OgelGames <olliverdc28@gmail.com>
2026-08-08 22:43:05 +1000

    HUD: Support short descriptions and simplify code

--- item_names.lua
+++ item_names.lua
@@ -42,7 +42,8 @@ end)
 
 core.register_globalstep(function(dtime)
 	for _, player in pairs(core.get_connected_players()) do
-		local data = item_names[player:get_player_name()]
+		local name = player:get_player_name()
+		local data = item_names[name]
 		if not data or not data.hud then
 			data = {} -- Update on next step
 			set_hud(player)
@@ -63,23 +64,23 @@ core.register_globalstep(function(dtime)
 			data.itemname = itemname
 			data.index = index
 			data.dtime = 0
-			local lang_code = core.get_player_information(player:get_player_name()).lang_code
+			local lang_code = core.get_player_information(name).lang_code
 
-			local desc = stack.get_meta
-				and stack:get_meta():get_string("description")
-
-			if not desc or desc == "" then
-				-- Try to use default description when none is set in the meta
-				local def = core.registered_items[itemname]
-				desc = def and def.description or ""
-			end
-			if only_names and desc and string.find(desc, "\n") then
-				desc = string.match(desc, "([^\n]*)")
+			local desc = stack:get_description()
+			if only_names then
+				-- Don't use 'stack:get_short_description()' because it breaks multi-line translations
+				local short = stack:get_meta():get("short_description") or stack:get_definition().short_description
+				if short and #short > 0 then
+					desc = short
+				end
 			end
 			desc = core.get_translated_string(lang_code, desc)
 			desc = core.strip_colors(desc)
-			if string.len(desc) > max_length and max_length > 0 then
-				desc = string.sub(desc, 1, max_length) .. " [...]"
+			if only_names and desc:find("\n") then
+				desc = desc:match("([^\n]*)")
+			end
+			if max_length > 0 and #desc > max_length then
+				desc = desc:sub(1, max_length) .. " [...]"
 			end
 			player:hud_change(data.hud, 'text', desc)
 		end
